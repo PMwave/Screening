e818abebc4b2e9e69f0c19062f380d8f17dd1c18
PMwave <108811532+PMwave@users.noreply.github.com>
2023-02-02 11:40:16 +0100

    Update clusters.py

--- clusters.py
+++ clusters.py
@@ -18,7 +18,9 @@ sns.set(rc={'figure.figsize':(13,9)})
 palette = sns.hls_palette(450, l=.4, s=.9)
 
 # plot
+fig = plt.figure()
 sns.scatterplot(X_embedded[:,0], X_embedded[:,1], hue=y_pred, legend='full', palette=palette)
 plt.title('t-SNE with Kmeans Labels')
+st.pyplot(fig)
 plt.savefig("improved_cluster_tsne.png")
 plt.show()
