0b4516ca97b7531502c3e2e464cc04436f065db0
PMwave <108811532+PMwave@users.noreply.github.com>
2023-02-02 11:34:34 +0100

    Create clusters.py

---
+++ clusters.py
@@ -0,0 +1,24 @@
+import numpy as np
+import streamlit as st
+
+with open('X_embedded.npy', 'rb') as f:
+    X_embedded = np.load(f)
+
+with open('y_pred.npy', 'rb') as f:
+    y_pred = np.load(f)
+    
+%matplotlib inline
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# sns settings
+sns.set(rc={'figure.figsize':(13,9)})
+
+# colors
+palette = sns.hls_palette(450, l=.4, s=.9)
+
+# plot
+sns.scatterplot(X_embedded[:,0], X_embedded[:,1], hue=y_pred, legend='full', palette=palette)
+plt.title('t-SNE with Kmeans Labels')
+plt.savefig("improved_cluster_tsne.png")
+plt.show()
